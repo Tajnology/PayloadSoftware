f276d96dcca153e6d8003cea9c8c97369ec38014
Taj <tajnology@gmail.com>
2021-10-04 11:32:37 +1000

    Added an aruco marker detection function

---
+++ target_detection/detect.py
@@ -0,0 +1,56 @@
+#### EXTERNAL MODULE IMPORTS ####
+import cv2
+
+# define names of each possible ArUco tag OpenCV supports
+ARUCO_DICT = {
+	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+	"DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+	"DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+	"DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+	"DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+	"DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+	"DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+	"DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+	"DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+	"DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+	"DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+	"DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+	"DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+	"DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+	"DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+	"DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+	"DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+	"DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+	"DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+}
+
+# Returns an array of tuples representing the corners of an aruco marker
+# (starting at top left and moving clockwise) or None if no aruco
+# marker was detected 
+def aruco_marker(frame,type):
+    arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[type])
+    arucoParams = cv2.aruco.DetectorParameters_create()
+    (corners, ids, rejected) = cv2.aruco.detectMarkers(frame, arucoDict, parameters = arucoParams)
+
+    if len(corners) > 0:
+        ids = ids.flatten()
+
+        markerBounds = []
+
+        for (markerCorner,markerID) in zip(corners,ids):
+            # Extract the marker corners (top left, top right, bottom right, bottom left)
+            # and append the tuple to the output array
+            markerBounds.append(markerCorner.reshape(4,2))
+
+        return markerBounds
+    else:
+        return None
+
+
+def body(frame):
+    pass
+
+def backpack(frame):
+    pass
