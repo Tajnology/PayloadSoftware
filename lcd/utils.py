67e18acfd2559a32211796b552848a940e38a2af
Taj <tajnology@gmail.com>
2021-10-05 11:25:30 +1000

    Finished get display mode function

--- lcd/utils.py
+++ lcd/utils.py
@@ -11,14 +11,12 @@ def get_ip():
         s.close()
     return IP
 
-DISPLAY_MODE_MAP = {'IP':0,'TARGET':1, 'TEMP':2}
+DISPLAY_MODE_MAP = {'IP':0, 'TARGET':1, 'TEMP':2}
 
 def get_display_mode(display_mode_file):
     file = open(display_mode_file,'r')
 
     display_mode_str = file.read().strip()
 
-    # TODO: Handle the string in the file 
-
-    return 0
+    return DISPLAY_MODE_MAP.get(display_mode_str,-1)
 
