54be4b60a9606280cf4e5f4c1e88179cc7f1d36b
shannonterris <shannonterriss@gmail.com>
2021-10-10 22:51:22 +1000

    get rid of circle

--- targetdetection/main.py
+++ targetdetection/main.py
@@ -59,25 +59,10 @@ def detect_aruco(frame, targets):
     if markers != None:
         for marker in markers:
             topLeft = marker['tl'], bottomRight = marker['br']
-            # topRight = marker['tr'], bottomLeft = marker['bl'], 
             # convert each of the (x, y)-coordinate pairs to integers
             topLeft = (int(topLeft[0]), int(topLeft[1]))
             bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
-            # topRight = (int(topRight[0]), int(topRight[1]))
-            # bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
-            # draw the bounding box of the ArUCo detection
-            # cv2.line(frame, topLeft, topRight, (0, 255, 0), 2)
-            # cv2.line(frame, topRight, bottomRight, (0, 255, 0), 2)
-            # cv2.line(frame, bottomRight, bottomLeft, (0, 255, 0), 2)
-            # cv2.line(frame, bottomLeft, topLeft, (0, 255, 0), 2)
-
             cv2.rectangle(frame, topLeft, bottomRight, (0,255,0), 2)
-
-            # compute and draw the center (x, y)-coordinates of the
-            # ArUco marker
-            cX = int((topLeft[0] + bottomRight[0]) / 2.0)
-            cY = int((topLeft[1] + bottomRight[1]) / 2.0)
-            cv2.circle(frame, (cX, cY), 4, (0, 0, 255), -1)
             # draw the ArUco marker ID on the frame
             cv2.putText(frame, str(marker['id']),
                 (topLeft[0], topLeft[1] - 15),
